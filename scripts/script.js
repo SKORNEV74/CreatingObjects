85df4349098ea66984e5215e0ff32e9cdc6dbd20
SKORNEV74 <yasher7495@yandex.ru>
2022-10-29 00:49:28 +0300

    constructor to be continued

--- scripts/script.js
+++ scripts/script.js
@@ -19,6 +19,13 @@ function Dog(name, breed, weight) {
     this.name = name;
     this.breed = breed;
     this.weight = weight;
+    this.bark = function () {
+        if (this.weight > 25) {
+            alert(this.name + " says Woof!");
+        } else {
+            alert(this.name + " says Yip!");
+        }
+    };
 }
 
 let fIdo = new Dog("Fido", "Mixed", 38);
@@ -34,4 +41,58 @@ for (let i = 0; i < dogs.length; i++) {
     }
 
     console.log("Dog: " + dogs[i].name + " is a " + size + " " + dogs[i].breed);
+
+    dogs[i].bark();
+}
+
+function Coffee(roast, ounces) {
+    this.roast = roast;
+    this.ounces = ounces;
+
+    this.getSize = function () {
+        if (this.ounces === 8) {
+            return "small";
+        } else if (this.ounces === 12) {
+            return "medium";
+        } else if (this.ounces === 16) {
+            return "large";
+        }
+    };
+    this.toString = function () {
+        return ("You've ordered a " + this.getSize() + " " + this.roast + " coffee.");
+    };
+}
+
+let houseBlend = new Coffee("House Blend", 12);
+console.log(houseBlend.toString());
+
+let darkRoast = new Coffee("Dark Roast", 16);
+console.log(darkRoast.toString());
+
+let capusino = new Coffee("Capusino", 8);
+console.log(capusino.toString());
+
+function Car(make, model, year, color, passengers, convertible, mileage) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.passengers = passengers;
+    this.convertible = convertible;
+    this.mileage = mileage;
+    this.started = false;
+
+    this.start = function () {
+        this.started = true;
+    }
+    this.drive = function () {
+        if (this.started) {
+            console.log(this.make + " " + this.model + " goes zoom zoom!");
+        } else {
+            console.log("Start the engine first.")
+        }
+    }
+    this.stop = function () {
+        this.started = false;
+    }
 }
